e905f11a1fdb58dc58951f4edc1f0b3bf6a4418a
moist_air <jdswee@163.com>
2020-12-07 23:44:08 +0800

    feat 首页搜索条布局

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <van-row gutter="20">
-      <van-col span="8">span: 8</van-col>
-      <van-col span="8">span: 8</van-col>
-      <van-col span="8">span: 8</van-col>
-    </van-row>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">
+          <img :src="location" width="45%" class="search-icon" />
+        </van-col>
+        <van-col span="15">
+          <input type="text" class="search-input" placeholder="查找">
+        </van-col>
+        <van-col span="6">
+          <van-button size="mini" class="search-button">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
   </div>
 </template>
 
@@ -12,17 +20,63 @@
   export default {
     data() {
       return {
-        msg: 'ShoppingMall'
+        msg: 'ShoppingMall',
+        location: require('../../assets/location.png')
       }
     },
   }
 </script>
 
 <style scoped>
-  .van-col {
-    margin-bottom: 1rem;
-    text-align: center;
-    background: #39a9ed;
-    background-clip: content-box;
+  .search-bar {
+    height: 2.2rem;
+    padding-bottom: 3px;
+    line-height: 2.2rem;
+    overflow: hidden;
+    background: #e5017d;
+  }
+  .search-icon {
+    padding-top: .6rem;
+    padding-left: .6rem;
+  }
+  .search-input {
+    width: 100%;
+    height: 1.4rem;
+    font-size: .7rem;
+    padding-left: .2rem;
+    color: #fff;
+    border-left: 0;
+    border-top: 0;
+    border-right: 0;
+    border-bottom: 1px solid #fff;
+    background: #e5017d;
+  }
+  input::-webkit-input-placeholder {
+    /* WebKit browsers */
+    color: #999;
+    opacity: 1;
+  }
+  input:-moz-placeholder {
+    /* Mozilla Firefox 4 to 18 */
+    color: #999;
+    opacity: 1;
+  }
+  input::-moz-placeholder {
+    /* Mozilla Firefox 19+ */
+    color: #999;
+    opacity: 1;
+  }
+  input:-ms-input-placeholder {
+    /* Internet Explorer 10+ */
+    color: #999;
+    opacity: 1;
+  }
+  .search-button {
+    width: 70%;
+    margin-left: .8rem;
+    color: #000;
+    border-color: #999;
+    border-radius: 5px;
+    background: #999;
   }
 </style>
